dd4e15a0ac27fbccabd0d33cd89e60de133c9514
Giang Le <gianghl2@illinois.edu>
2021-02-16 17:49:50 +0000

    Some modifications

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # wmt2021
 Repository for WMT 2021
+
+# Get Data
+./scripts/get_data.sh -c paracrawl
+
+# Unzipping data and splitting to train, dev, test
+./scripts/preprocess_data.sh
--- scripts/preprocess_data.sh
+++ scripts/preprocess_data.sh
@@ -5,6 +5,7 @@
 DIR=`dirname "$0"`
 BASE=$DIR/..
 
+if [ -f $BASE/data/reuters-ja-en.txt.gz ]; then
 echo "Preparing reuters data as train data..."
 gunzip $BASE/data/reuters-ja-en.txt.gz
 iconv -f EUC-JP -t UTF-8 < $BASE/data/reuters-ja-en.txt > $BASE/data/reuters-ja-en-decoded.txt
@@ -28,7 +29,10 @@ cat $BASE/data/reuters-ja-total.txt | tail -n $test_count > $BASE/data/test-ja-r
 cat $BASE/data/reuters-en-total.txt | tail -n $test_count > $BASE/data/test-en-raw.txt
 LC_TEST_EN=$(wc -l < $BASE/data/test-en-raw.txt)
 LC_TEST_JA=$(wc -l < $BASE/data/test-ja-raw.txt)
-
+else
+	echo "Skipping Reuters data processing because no data were found."
+	exit
+fi
 #echo "Preparing Kyoto Free Translation Task data as test data..."
 #tar -xzf $BASE/data/kyoto-en-ja.tar.gz
 #mv $BASE/kftt-data-1.0/data/orig/kyoto-test.en $BASE/data/test-en-raw.txt
@@ -36,6 +40,7 @@ LC_TEST_JA=$(wc -l < $BASE/data/test-ja-raw.txt)
 #LC_TEST_EN=$(wc -l < $BASE/data/test-en-raw.txt)
 #LC_TEST_JA=$(wc -l < $BASE/data/test-ja-raw.txt)
 
+if [ -f $BASE/data/paracrawl-en-ja.tar.gz ]; then
 echo "Preparing Paracrawl data as full data..."
 tar -xzf $BASE/data/paracrawl-en-ja.tar.gz
 mv $BASE/en-ja/en-ja.bicleaner05.txt $BASE/data/paracrawl-en-ja.txt
@@ -66,6 +71,11 @@ cat $BASE/data/paracrawl-en-shortened.txt | tail -n $test_para_count > $BASE/dat
 LC_TEST_PARA_EN=$(wc -l < $BASE/data/test-para-en-raw.txt)
 LC_TEST_PARA_JA=$(wc -l < $BASE/data/test-para-ja-raw.txt)
 
+else
+	echo "Skipping Paracrawl data processing because no data were found."
+	exit
+fi
+
 echo "Total number of JA REUTERS TRAIN sentences is $LC_TRAIN_JA"
 echo "Total number of EN REUTERS TRAIN sentences is $LC_TRAIN_EN"
 echo "Total number of JA REUTERS DEV sentences is $LC_DEV_JA"
@@ -80,16 +90,16 @@ echo "Total number of JA PARACRAWL TEST sentences is $LC_TEST_PARA_JA"
 echo "Total number of EN PARACRAWL TEST sentences is $LC_TEST_PARA_EN"
 
 # Do copying for baseline and experiments for Japanese
-mv $BASE/data/train-ja-raw.txt $BASE/data/train-bl-ja-raw.txt
-cp $BASE/data/train-bl-ja-raw.txt $BASE/data/train-exp-ja-raw.txt
-mv $BASE/data/dev-ja-raw.txt $BASE/data/dev-bl-ja-raw.txt
-cp $BASE/data/dev-bl-ja-raw.txt $BASE/data/dev-exp-ja-raw.txt
-mv $BASE/data/test-ja-raw.txt $BASE/data/test-bl-ja-raw.txt
-cp $BASE/data/test-bl-ja-raw.txt $BASE/data/test-exp-ja-raw.txt
+#mv $BASE/data/train-ja-raw.txt $BASE/data/train-bl-ja-raw.txt
+#cp $BASE/data/train-bl-ja-raw.txt $BASE/data/train-exp-ja-raw.txt
+#mv $BASE/data/dev-ja-raw.txt $BASE/data/dev-bl-ja-raw.txt
+#cp $BASE/data/dev-bl-ja-raw.txt $BASE/data/dev-exp-ja-raw.txt
+#mv $BASE/data/test-ja-raw.txt $BASE/data/test-bl-ja-raw.txt
+#cp $BASE/data/test-bl-ja-raw.txt $BASE/data/test-exp-ja-raw.txt
 
-mv $BASE/data/train-para-ja-raw.txt $BASE/data/train-para-bl-ja-raw.txt
-cp $BASE/data/train-para-bl-ja-raw.txt $BASE/data/train-para-exp-ja-raw.txt
-mv $BASE/data/dev-para-ja-raw.txt $BASE/data/dev-para-bl-ja-raw.txt
-cp $BASE/data/dev-para-bl-ja-raw.txt $BASE/data/dev-para-exp-ja-raw.txt
-mv $BASE/data/test-para-ja-raw.txt $BASE/data/test-para-bl-ja-raw.txt
-cp $BASE/data/test-para-bl-ja-raw.txt $BASE/data/test-para-exp-ja-raw.txt
+#mv $BASE/data/train-para-ja-raw.txt $BASE/data/train-para-bl-ja-raw.txt
+#cp $BASE/data/train-para-bl-ja-raw.txt $BASE/data/train-para-exp-ja-raw.txt
+#mv $BASE/data/dev-para-ja-raw.txt $BASE/data/dev-para-bl-ja-raw.txt
+#cp $BASE/data/dev-para-bl-ja-raw.txt $BASE/data/dev-para-exp-ja-raw.txt
+#mv $BASE/data/test-para-ja-raw.txt $BASE/data/test-para-bl-ja-raw.txt
+#cp $BASE/data/test-para-bl-ja-raw.txt $BASE/data/test-para-exp-ja-raw.txt
