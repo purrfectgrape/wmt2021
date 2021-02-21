6624567aa1e0b2ce24b44c173b73dd486c47898d
Giang Le <gianghl2@illinois.edu>
2021-02-21 15:21:38 +0000

    update buildvocab

--- scripts/build_vocab.sh
+++ scripts/build_vocab.sh
@@ -12,13 +12,13 @@ while getopts ":c:" opt; do
   case $opt in
     c)
       echo "Build vocab for mixed scripts"
-      onmt_preprocess -train_src $BASE/data/train-$OPTARG-tok-bpe.ja -train_tgt $BASE/data/train-$OPTARG-tok-bpe.en -valid_src $BASE/data/dev-$OPTARG-tok-bpe.ja -valid_tgt $BASE/data/dev-tok-bpe.en -save_data $BASE/data/OpenNMT-data-$OPTARG -overwrite
+      onmt_preprocess -train_src //nas/models/experiment/ja-en/wmt2021/data/train-$OPTARG-tok-bpe.ja -train_tgt //nas/models/experiment/ja-en/wmt2021/data/train-$OPTARG-tok-bpe.en -valid_src //nas/models/experiment/ja-en/wmt2021/data/dev-$OPTARG-tok-bpe.ja -valid_tgt //nas/models/experiment/ja-en/wmt2021/data/dev-tok-bpe.en -save_data //nas/models/experiment/ja-en/wmt2021/data/OpenNMT-data-$OPTARG -overwrite
     
       echo "Build vocab for hiragana"
-      onmt_preprocess -train_src $BASE/data/train-$OPTARG-tok-bpe-hiragana.ja -train_tgt $BASE/data/train-$OPTARG-tok-bpe-hiragana.en -valid_src $BASE/data/dev-$OPTARG-tok-bpe-hiragana.ja -valid_tgt $BASE/data/dev-tok-bpe.en -save_data $BASE/data/OpenNMT-data-$OPTARG-hiragana -overwrite
+      onmt_preprocess -train_src //nas/models/experiment/ja-en/wmt2021/data/train-$OPTARG-tok-bpe-hiragana.ja -train_tgt //nas/models/experiment/ja-en/wmt2021/data/train-$OPTARG-tok-bpe-hiragana.en -valid_src //nas/models/experiment/ja-en/wmt2021/data/dev-$OPTARG-tok-bpe-hiragana.ja -valid_tgt //nas/models/experiment/ja-en/wmt2021/data/dev-tok-bpe.en -save_data //nas/models/experiment/ja-en/wmt2021/data/OpenNMT-data-$OPTARG-hiragana -overwrite
       
       echo "Build vocab for romaji"
-      onmt_preprocess -train_src $BASE/data/train-$OPTARG-tok-bpe-romaji.ja -train_tgt $BASE/data/train-$OPTARG-tok-bpe-romaji.en -valid_src $BASE/data/dev-$OPTARG-tok-bpe-romaji.ja -valid_tgt $BASE/data/dev-tok-bpe.en -save_data $BASE/data/OpenNMT-data-$OPTARG-romaji -overwrite
+      onmt_preprocess -train_src //nas/models/experiment/ja-en/wmt2021/data/train-$OPTARG-tok-bpe-romaji.ja -train_tgt //nas/models/experiment/ja-en/wmt2021/data/train-$OPTARG-tok-bpe-romaji.en -valid_src //nas/models/experiment/ja-en/wmt2021/data/dev-$OPTARG-tok-bpe-romaji.ja -valid_tgt //nas/models/experiment/ja-en/wmt2021/data/dev-tok-bpe.en -save_data //nas/models/experiment/ja-en/wmt2021/data/OpenNMT-data-$OPTARG-romaji -overwrite
     
        ;;
     \?)
