760e437dd6968246ff7e15fe681298818b44641e
Giang Le <gianghl2@illinois.edu>
2021-02-21 09:09:36 -0600

    build vocab

--- scripts/build_vocab.sh
+++ scripts/build_vocab.sh
@@ -12,13 +12,13 @@ while getopts ":c:" opt; do
   case $opt in
     c)
       echo "Build vocab for mixed scripts"
-      onmt_preprocess -train_src ~/project-scripts-purrfectgrape/data/train-$OPTARG-tok-bpe.ja -train_tgt ~/project-scripts-purrfectgrape/data/train-$OPTARG-tok-bpe.en -valid_src ~/project-scripts-purrfectgrape/data/dev-$OPTARG-tok-bpe.ja -valid_tgt ~/project-scripts-purrfectgrape/data/dev-tok-bpe.en -save_data ~/project-scripts-purrfectgrape/data/OpenNMT-data-$OPTARG -overwrite
+      onmt_preprocess -train_src $BASE/data/train-$OPTARG-tok-bpe.ja -train_tgt $BASE/data/train-$OPTARG-tok-bpe.en -valid_src $BASE/data/dev-$OPTARG-tok-bpe.ja -valid_tgt $BASE/data/dev-tok-bpe.en -save_data $BASE/data/OpenNMT-data-$OPTARG -overwrite
     
       echo "Build vocab for hiragana"
-      onmt_preprocess -train_src ~/project-scripts-purrfectgrape/data/train-$OPTARG-tok-bpe-hiragana.ja -train_tgt ~/project-scripts-purrfectgrape/data/train-$OPTARG-tok-bpe-hiragana.en -valid_src ~/project-scripts-purrfectgrape/data/dev-$OPTARG-tok-bpe-hiragana.ja -valid_tgt ~/project-scripts-purrfectgrape/data/dev-tok-bpe.en -save_data ~/project-scripts-purrfectgrape/data/OpenNMT-data-$OPTARG-hiragana -overwrite
+      onmt_preprocess -train_src $BASE/data/train-$OPTARG-tok-bpe-hiragana.ja -train_tgt $BASE/data/train-$OPTARG-tok-bpe-hiragana.en -valid_src $BASE/data/dev-$OPTARG-tok-bpe-hiragana.ja -valid_tgt $BASE/data/dev-tok-bpe.en -save_data $BASE/data/OpenNMT-data-$OPTARG-hiragana -overwrite
       
       echo "Build vocab for romaji"
-      onmt_preprocess -train_src ~/project-scripts-purrfectgrape/data/train-$OPTARG-tok-bpe-romaji.ja -train_tgt ~/project-scripts-purrfectgrape/data/train-$OPTARG-tok-bpe-romaji.en -valid_src ~/project-scripts-purrfectgrape/data/dev-$OPTARG-tok-bpe-romaji.ja -valid_tgt ~/project-scripts-purrfectgrape/data/dev-tok-bpe.en -save_data ~/project-scripts-purrfectgrape/data/OpenNMT-data-$OPTARG-romaji -overwrite
+      onmt_preprocess -train_src $BASE/data/train-$OPTARG-tok-bpe-romaji.ja -train_tgt $BASE/data/train-$OPTARG-tok-bpe-romaji.en -valid_src $BASE/data/dev-$OPTARG-tok-bpe-romaji.ja -valid_tgt $BASE/data/dev-tok-bpe.en -save_data $BASE/data/OpenNMT-data-$OPTARG-romaji -overwrite
     
        ;;
     \?)
