dd5ceca96624e0db933b4b16a34c917032b67177
Giang Le <gianghl2@illinois.edu>
2021-02-21 04:04:31 +0000

    done

--- scripts/prepare_paracrawl_sents.py
+++ scripts/prepare_paracrawl_sents.py
@@ -8,10 +8,11 @@ import re
 import os
 import sys
 
+os.chdir("..")
 data_dir = os.getcwd() + "/data"
 
-if not len(sys.argv) == 3:
-    raise SystemExit("Usage: {} input_file output_file".format(sys.argv[0]))
+if not len(sys.argv) == 4:
+    raise SystemExit("Usage: {} input_file ja_outfile en_outfile".format(sys.argv[0]))
 
 if not "paracrawl-en-ja.txt" in sys.argv[1]:
     raise SystemExit("The first parameter contains paracrawl-en-ja.txt")
@@ -36,9 +37,33 @@ def write_to_file(outfile, sents_dict):
     with open(outfile,"w") as out:
         for k in sents_dict.keys():
             if float(k[1]) > 0.790:
-                out.write(k[0] + "\t" + sents_dict[k] + "\n")
-    
+                out.write("{}\t{}\n".format(k[0], sents_dict[k]))
+
+# Create a list of Japanese sentences and English sentences
+def create_sent_lists(filename):
+    ja_sents = []
+    en_sents = []
+    with open(filename) as infile:
+        for line in infile:
+            # Skip blank lines.
+            if line.rstrip():
+                en_sents.append(line.strip().split("\t")[0])
+                ja_sents.append(line.strip().split("\t")[1])
+        return ja_sents, en_sents
+
+def write_to_separate(filename, sent_list):
+    with open(filename, "w") as outfile:
+        for sent in sent_list:
+            outfile.write(sent + "\n")
+
 if __name__ == "__main__":
-    outfile = sys.argv[2]
+    ja_outfile = sys.argv[2]
+    en_outfile = sys.argv[3]
     mydict = create_dict(sys.argv[1])
-    write_to_file(outfile, mydict)
+    tmp_file = data_dir + "/tmp"
+    write_to_file(tmp_file, mydict)
+    ja_sents, en_sents = create_sent_lists(tmp_file)
+    write_to_separate(ja_outfile, ja_sents)
+    write_to_separate(en_outfile, en_sents)
+    os.remove(tmp_file)
+
