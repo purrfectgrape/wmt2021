2934f83917ee58af4bffa9f241c57ffb488d929e
Giang Le <gianghl2@illinois.edu>
2021-04-05 11:03:37 +0000

    updated

--- scripts/pofo_tagger.py
+++ scripts/pofo_tagger.py
@@ -5,12 +5,14 @@
 
 # Politeness and formality tagger for English -> Japanese translation.
 """
+from __future__ import division
 import Mykytea
 import os
 import sys
 import argparse
 import re
 import difflib
+
 
 ###############################################################################
 #
@@ -150,6 +152,7 @@ def longest_match(verb_ending, politeness_formality_mappings, line_ja):
             size_of_match = s.find_longest_match(0, len(ending), 0, len(verb_ending))[2]
             if size_of_match != 0:
                 matches_dict[line_ja] = (verb_ending, ending, size_of_match)
+    print(matches_dict)
     return matches_dict
 
 def revised_write_tags(verb_ending, ending, line_ja, line_en, out_en):
@@ -160,11 +163,13 @@ def revised_write_tags(verb_ending, ending, line_ja, line_en, out_en):
 
 def write_tags(verb_ending, politeness_formality_mappings, line_ja, line_en, out_en):
     tagged = False
+    tag = ''
     for key, endings in politeness_formality_mappings.items():
         for ending in endings:
             if verb_ending.endswith(ending):
                 out_en.write(re.sub('^', '<' + key + '> ', line_en).strip() + '\n')
                 tagged = True
+                tag = key
                 print('TAGGED AS ' + key + ' || ' +  verb_ending + ' || ' + line_ja)
                 break
         else:
@@ -172,11 +177,15 @@ def write_tags(verb_ending, politeness_formality_mappings, line_ja, line_en, out
     if tagged == False:
         print('NO TAG || ' + verb_ending + ' || ' + line_ja) 
         out_en.write(line_en.strip() + '\n')
-    return tagged
+    return (tagged, tag)
 
 nl = 0
-count = 0
-true_count = 0
+counts = ()
+count_tagged = 0
+count_untagged = 0
+count_plain = 0
+count_polite = 0
+count_formal = 0
 with open(args.corpus + '-pofo-tagged.en', 'wt') as out_en:
     with open(args.corpus + '.en', 'rt') as f_en:
         with open(args.corpus + '.ja', 'rt') as f_ja:
@@ -185,11 +194,30 @@ with open(args.corpus + '-pofo-tagged.en', 'wt') as out_en:
                 line_ja = f_ja.readline()
                 line_ja = clean_up(line_ja)
                 verb_ending = extract_verb_ending(line_ja)
-                count += write_tags(verb_ending, politeness_formality_mappings, line_ja, line_en, out_en)
-                print(longest_match(verb_ending, politeness_formality_mappings, line_ja))
+                counts += write_tags(verb_ending, politeness_formality_mappings, line_ja, line_en, out_en)
+                #longest_match(verb_ending, politeness_formality_mappings, line_ja)
                 nl += 1
 
-print('Number of lines tagged: ' + str(count))
+for i in counts:
+    if i == True:
+        count_tagged += 1
+    elif i == 'plain':
+        count_plain += 1
+    elif i == 'polite':
+        count_polite += 1
+    elif i == 'formal':
+        count_formal += 1
+    elif i == False:
+        count_untagged += 1
+
+print('Number of lines tagged: ' + str(count_tagged))
+print('Percentage of lines tagged: ' + '{0:.2f}%'.format(count_tagged/args.nb_sents * 100))
+print('Number of lines untagged: ' + str(count_untagged))
+print('Percentage of lines untagged: ' + '{0:.2f}%'.format(count_untagged/args.nb_sents * 100))
+print('Percentage of lines tagged plain: ' + '{0:.2f}%'.format(count_plain/args.nb_sents * 100))
+print('Percentage of lines tagged polite: ' + '{0:.2f}%'.format(count_polite/args.nb_sents * 100))
+print('Percentage of lines tagged formal: ' + '{0:.2f}%'.format(count_formal/args.nb_sents * 100))
+
 if sum(1 for line in open(args.corpus + '-pofo-tagged.en')) != args.nb_sents:
     print('WARNING!!! Lines count in ' +  args.corpus + '-pofo-tagged.en' + ' is not equal to args.nb_sents!!. Likely due to double tagging.')
 else:
